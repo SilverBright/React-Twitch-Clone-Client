9d25bdf7460f3d2969c8329d08d2518d3989a483
github username <github email address>
2019-05-23 16:21:25 -0700

    stub out App with basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+    </div>
+  );
+};
 
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+  </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
-}
+  return (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pageTwo" exact component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  </div>
+  );
+};
 
 export default App;
