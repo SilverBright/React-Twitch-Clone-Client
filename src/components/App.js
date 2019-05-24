735e48db48d7c010a25a69c18526ab23fba987c9
github username <github email address>
2019-05-23 20:14:15 -0700

    add routing for Header, import some stylesheets for styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
-
-
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pageTwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two 
-      <Link to="/">Navigate to Page One</Link>
-  </div>
-  );
-};
+import Header from './Header';
 
 const App = () => {
   return (
-  <div>
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
-        <Route path="/streams/show" exact component={StreamShow} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
-      </div>
-    </BrowserRouter>
-  </div>
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
