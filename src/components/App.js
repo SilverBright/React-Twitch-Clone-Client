f4e8baf42eed0ac21d4ea12b0f8b2fc58361f2e2
github username <github email address>
2019-05-23 19:56:03 -0700

    add scaffolding for all routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
 
 const PageOne = () => {
   return (
@@ -24,8 +30,11 @@ const App = () => {
   <div>
     <BrowserRouter>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pageTwo" exact component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
       </div>
     </BrowserRouter>
   </div>
