4e2ccc7df238dfc5aa1f20ee50d187bb9fae59df
github username <github email address>
2019-05-26 13:28:39 -0700

    refactor StreamEdit to show title and description property

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { fetchStream } from '../../actions';
+import { tsPropertySignature } from '@babel/types';
+import { fetchStream, editStream } from '../../actions';
 import { connect } from 'react-redux';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component  {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     // console.log(this.props);
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit A Stream</h3>
+        <StreamForm 
+        // initialValues is a special redux-form helper function 
+          initialValues={this.props.stream} onSubmit={this.OnSubmit} />
+      </div>
+    );
   }
 }
 
@@ -22,4 +35,4 @@ const mapStateToProps = (state, ownProps) => {
 
 
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream })(StreamEdit);
