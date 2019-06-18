42d56dde4420d997b2d7a4f62b725b8493346ff1
github username <barbara.m.washington@gmail.com>
2019-06-17 21:39:41 -0700

    import fetchStreams and connect to StreamShow component, render title and description props for a stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import streams from '../../apis/streams';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
-}
+class StreamShow extends React.Component {
 
-export default StreamShow;
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading stream...</div>;
+    }
+
+      return (
+        <div>
+          <h1>{this.props.stream.title}</h1>
+          <h5>{this.props.stream.description}</h5>
+        </div>
+      );
+    }
+  }
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
