34ce04037b6aa3929913f0082e45052275c50ca4
github username <barbara.m.washington@gmail.com>
2019-07-19 20:24:24 -0700

    add componentWillMount function to stop video player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -22,6 +22,10 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillMount() {
+    this.player.destroy();
+  }
+
   buildPlayer() {
     if (this.player || !this.props.stream) {
       return;
